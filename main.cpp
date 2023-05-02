2fbeb5a4584547b974a8039a80fdb4deb813d820
Junkynioy <virgil_bravo@tahoo.com>
2023-05-02 15:07:19 +0800

    basics

--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 using namespace std;
 
+int print(string inputString, float inputfloat=0) {
+    cout << inputString << inputfloat << "\n";
+}
+
 int main() {
-  cout << "Hello World!";
-  return 0;
+    string x; float y;
+    print("Test Gaming",1);
+    print("Test Gaming",2);
+    cout << "input a string >> ";
+    cin >> x;
+    cout << "input a float >> ";
+    cin >> y;
+    print(x,y);
+    return 0;
 }
