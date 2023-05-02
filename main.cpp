dd8db910e94f7666a9525d58d2711ead145fd80d
Junkynioy <virgil_bravo@tahoo.com>
2023-05-02 15:25:13 +0800

    i accidentally remade 2 py functions :skull:

--- main.cpp
+++ main.cpp
@@ -5,12 +5,25 @@ int print(string inputString, float inputfloat=0) {
     cout << inputString << inputfloat << "\n";
 }
 
+// int inputInt(string message) {
+//     int intValue;
+//     cout << message;
+//     getline (cin, intValue);
+//     return intValue;
+// }
+
+string inputStr(string message="") {
+    string strValue;
+    cout << message;
+    getline (cin, strValue);
+    return strValue;
+}
+
 int main() {
     string x; float y;
     print("Test Gaming",1);
-    print("Test Gaming",2);
-    cout << "input a string >> ";
-    cin >> x;
+    print("Test Gaming",2); // test recreation of print() from py
+    x = inputStr("Insert String Value >> "); // recreation of input() from py, however this is specific to one data type
     cout << "input a float >> ";
     cin >> y;
     print(x,y);
